6fe9e8d4e2d1858a3cbbb6a8274e16bc78a6fe7e
Shibi <shibisuriya.work@gmail.com>
2024-11-28 19:52:05 +0530

    two sum dummy

---
+++ leetcode/two_sum/two_sum_2/input_parser.hpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Parser {
+     public:
+      pair<string, int> parse() { return std::make_pair("Hello", 124); }
+};
---
+++ leetcode/two_sum/two_sum_2/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#include "./input_parser.hpp"
+#include "solution.hpp"
+
+using namespace std;
+
+int main() {
+      Parser parser;
+      pair<string, int> input = parser.parse();
+      Solution solution;
+      return 0;
+}
