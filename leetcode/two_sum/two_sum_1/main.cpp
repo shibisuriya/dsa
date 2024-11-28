7cd541bcd231e415592e270adbdd39800e2cb9b7
Shibi <shibisuriya.work@gmail.com>
2024-11-28 20:09:00 +0530

    two sum brute force

--- leetcode/two_sum/two_sum_1/main.cpp
+++ leetcode/two_sum/two_sum_1/main.cpp
@@ -1,22 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-#include "input_parser.h"
+#include <utility>
 
 using namespace std;
 
+pair<vector<int>, int> parse_input() {
+      string line = "";
+      getline(cin, line);
+      istringstream iss(line);
+      int temp = 0;
+      vector<int> nums;
+      while (iss >> temp) {
+            nums.push_back(temp);
+      }
+      int target = 0;
+      cin >> target;
+
+      return make_pair(nums, target);
+}
+
 class Solution {
      public:
-      vector<int> twoSum(string a, int target) { return vector<int>{1, 2, 3}; }
+      vector<int> twoSum(vector<int>& nums, int target) {
+            for (int i = 0; i < nums.size() - 1; i++) {
+                  for (int j = i + 1; j < nums.size(); j++) {
+                        if (nums[i] + nums[j] == target) {
+                              return vector<int>{i, j};
+                        }
+                  }
+            }
+            return vector<int>{};
+      }
 };
 
 int main() {
       Solution solution;
-      Parser parser;
-      pair<string, int> inputs = parser.parse();
+      pair<vector<int>, int> inputs = parse_input();
       vector<int> answer = solution.twoSum(inputs.first, inputs.second);
+
+      cout << "Answer => ";
       for (int a : answer) {
             cout << a << " ";
       }
+      cout << endl;
 
       return 0;
 }
