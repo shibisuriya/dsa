9b45d4312d2aee4ab68999d8be74150464252812
Shibi <shibisuriya.work@gmail.com>
2024-12-01 15:23:38 +0530

    unordered maps

---
+++ stl_practice/unordered_maps/main.cpp
@@ -0,0 +1,7 @@
+// # Unordered maps
+// - Unordered maps and maps have the same inteface.
+// - Unordered maps don't keep their keys sorted... The are implemented using hash tables
+// not trees.
+// - The time complexity of reading/writing an item in an unordered map is
+// O(1).
+// Only data types that have a native hashing function available can be used as keys in an unordered_map.
