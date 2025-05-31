38b90c137b105a1309c54287126622606c1a6aff
Shibi Suriya P <shibisuriya.work@gmail.com>
2025-05-31 13:33:14 +0530

    all three depth first traversals written in java

---
+++ trees/binary tree/Postorder Traversal/java/Main.java
@@ -0,0 +1,78 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+// Post order traversal
+// Left -> Right -> Root
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+    TreeNode root = null;
+    Queue<TreeNode> q = new LinkedList<>();
+    if (scanner.hasNextInt()) {
+      int val = scanner.nextInt();
+      if (val != -1) {
+        root = new TreeNode(val);
+        q.add(root);
+      }
+    }
+    while (!q.isEmpty()) {
+      TreeNode node = q.poll();
+      if (scanner.hasNextInt()) {
+        int val = scanner.nextInt();
+        if (val != -1) {
+          node.left = new TreeNode(val);
+          q.add(node.left);
+        }
+      }
+      if (scanner.hasNextInt()) {
+        int val = scanner.nextInt();
+        if (val != -1) {
+          node.right = new TreeNode(val);
+          q.add(node.right);
+        }
+      }
+    }
+    List<Integer> solution = new Solution().postorderTraversal(root);
+    for (Integer item : solution) System.out.print(item + "  ");
+  }
+}
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  List<Integer> solution = new ArrayList<>();
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    if (root == null) return solution;
+
+    postorderTraversal(root.left);
+
+    postorderTraversal(root.right);
+
+    solution.add(root.val);
+
+    return solution;
+  }
+}
